e54e4d037ef9307bddca778b462dd36113a1b534
suat kıvam <suatkivam@gmail.com>
2025-09-14 22:35:40 +0300

    Update pars.h: define get_coordinates struct

--- includes/pars.h
+++ includes/pars.h
@@ -7,25 +7,39 @@
 # include <fcntl.h>
 # include <stdio.h>
 
-#define c ".fdf"
+# define c ".fdf"
 # define MAP_ERR_INVALID -1
 /* map hatalı: satır uzunlukları tutmuyor veya boş file */
 # define MAP_ERR_OPEN -2 /* dosya açılamadı */
 
 typedef struct s_get_map_dimensions
 {
-	int		fd;
-	int		expected_columns;
-	int		line_num;
-	int		current_columns;
-	char	**split_lines;
-	char	*line;
-}			t_get_map_dimensions;
+	int					fd;
+	int					expected_columns;
+	int					line_num;
+	int					current_columns;
+	char				**split_lines;
+	char				*line;
+}						t_get_map_dimensions;
 
-const char	*ft_basename(const char *path);
-int			is_hidden_file_with_ext(const char *path, const char *extension);
+typedef struct s_get_coordinates
+{
+    int                 fd;
+    int                 width;
+    int                 height;
+    t_point_coordinate  *coordinates;
+    int                 row;
+    int                 index;
+    char                *line;
+} t_get_coordinates;
 
-int			open_file(const char *file_name);
-int			get_map_dimensions(const char *file_name);
+const char				*ft_basename(const char *path);
+int						is_hidden_file_with_ext(const char *path,
+							const char *extension);
 
+int						open_file(const char *file_name);
+int						get_map_dimensions(const char *file_name);
+
+t_point_coordinate		*get_coordinates(const char *file_name,
+							int *total_cells);
 #endif
