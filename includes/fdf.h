1c48a9907e623c7bffb85a77a488919ad2a72f06
suat kıvam <suatkivam@gmail.com>
2025-09-14 19:27:10 +0300

    Update fdf.h: define t_fdf_data and function prototypes

--- includes/fdf.h
+++ includes/fdf.h
@@ -7,6 +7,8 @@
 # include <fcntl.h>
 # include <math.h>
 
+# define MAP_EXTENSION ".fdf"
+
 # define WIN_WIDTH 1280
 # define WIN_HEIGHT 720
 typedef struct s_fdf_data
@@ -31,6 +33,8 @@ typedef struct s_fdf_data
 void		init_data(t_fdf_data *data);
 int			handle_key_press(int keycode, t_fdf_data *mlx);
 int			close_window(t_fdf_data *mlx);
+
+int			is_valid_map_file(const char *path);
 
 void		draw_line(t_fdf_data *fdf, t_line *line);
 void		render_grid(t_fdf_data *data);
