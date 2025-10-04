3947bb790d60fce6e75c4896ebb55350814d7eaa
suat kıvam <suatkivam@gmail.com>
2025-10-04 16:49:09 +0300

    main.c write for tests

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
+#include "libft.h"
 #include "parser.h"
-#include <stdio.h>
+#include "printf.h"
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s <map_file.fdf>\n", argv[0]);
-		return 1;
+		fprintf(2, "Usage: %s <map_file.fdf>\n", argv[0]);
+		return (1);
 	}
-
+	if (!is_valid_file(argv[1]))
+	{
+		fprintf(2, "Invalid map file (must end with %s): %s\n",
+			MAP_EXTENSION, argv[1]);
+		return (1);
+	}
 	/* TODO: call parser/load map here */
 	(void)argv;
-	return 0;
+	return (0);
 }
